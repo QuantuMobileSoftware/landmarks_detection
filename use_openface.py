995b3915834eb023f40277528e84de5ee5075b94
exotikh3 <dotavsegor@gmail.com>
2017-03-15 17:03:09 +0200

    stable

--- use_openface.py
+++ use_openface.py
@@ -4,18 +4,20 @@ import itertools
 import os
 import numpy as np
 
-img = cv2.imread('./pics/0000000000146780.jpg', 0)
+
 
 align = openface.AlignDlib('shape_predictor_68_face_landmarks.dat')
 
-bb_roi = align.getLargestFaceBoundingBox(img)
-
-landmarks = align.findLandmarks(img, bb_roi)
-
-for landmark in landmarks:
-    cv2.circle(img, landmark, radius=3, color=(0,0,255))
-
 with open("output.txt", "w") as text_file:
-    text_file.write("landmarks: \n {}".format(landmarks))
+    for filename in os.listdir('./pics'):
+        img = cv2.imread('./pics/'+filename, 0)
 
-cv2.imwrite('result.jpg', img)
+        bb_roi = align.getLargestFaceBoundingBox(img)
+
+        landmarks = align.findLandmarks(img, bb_roi)
+
+        for landmark in landmarks:
+            cv2.circle(img, landmark, radius=3, color=(0,0,255))
+
+        text_file.write("landmarks  {}: \n {} \n\n".format(filename, landmarks))
+        cv2.imwrite('./res_pics/res_'+filename, img)
